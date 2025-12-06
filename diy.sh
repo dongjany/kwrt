5b78f72599fbd9582889d38e71e322e9c652588d
dongjany <3145255636@qq.com>
2025-12-06 23:50:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,3 +3,4 @@ patch -p1 < patches/BRCMFMAC_SDIO.patch
 patch -p1 < patches/onecloud.patch
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/sbwml/luci-app-ramfree.git package/luci-app-ramfree
