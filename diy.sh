219abc7c69a28a59b59b2769b14d0dd031a1b98c
dongjany <3145255636@qq.com>
2025-12-06 22:49:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/dongjany/amlogic target/linux/
+git clone https://github.com/dongjany/amlogic.git target/linux/
 patch -p1 < ./BRCMFMAC_SDIO.patch
 patch -p1 < ./onecloud.patch
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/
