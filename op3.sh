01aa85943b7c0b139757e4a78e5a79d20c4c2dee
dongjany <3145255636@qq.com>
2025-12-07 10:35:38 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -1,3 +1,4 @@
 sed -i 's/192.168.1.1/192.168.2.15/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
