87c82c2bff7d379b87e5e665d8fb21f60c097076
dongjany <3145255636@qq.com>
2025-12-07 09:22:11 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -1,9 +1,2 @@
 git clone https://github.com/dongjany/amlogic.git target/linux/amlogic
-patch -p1 < patches/BRCMFMAC_SDIO.patch
-patch -p1 < patches/onecloud.patch
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/sbwml/luci-app-ramfree.git package/luci-app-ramfree
-sed -i 's/192.168.1.1/192.168.2.15/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
