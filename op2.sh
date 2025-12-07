fdf3e11350d5cd6fff2be10883938048443e7075
dongjany <3145255636@qq.com>
2025-12-07 09:20:22 +0800

    Update op2.sh

--- op2.sh
+++ op2.sh
@@ -1,1 +1,3 @@
-
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/sbwml/luci-app-ramfree.git package/luci-app-ramfree
